2ae39c33dd6accad581ecd68be5cb6bc1b14bca9
ChristianOcchionero <43983111+ChristianOcchionero@users.noreply.github.com>
2019-02-04 10:04:11 +0100

    Packs
    3 layer packs

--- src/schoolapp/SchoolApp.java
+++ src/schoolapp/SchoolApp.java
@@ -21,7 +21,7 @@ public class SchoolApp extends Application
     @Override
     public void start(Stage stage) throws Exception
     {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/schoolapp/gui/view/FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
         
