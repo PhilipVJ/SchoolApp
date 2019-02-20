b5286f3b8c89c078af89175e448454285f416141
PhilipVJ <44639834+PhilipVJ@users.noreply.github.com>
2019-02-20 11:37:07 +0100

    cssFix

--- src/schoolapp/gui/controller/StudentViewController.java
+++ src/schoolapp/gui/controller/StudentViewController.java
@@ -15,21 +15,17 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Side;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.chart.CategoryAxis;
-import javafx.scene.chart.Chart;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import schoolapp.be.Attendance;
@@ -169,12 +165,11 @@ public class StudentViewController implements Initializable
     private void openChart(MouseEvent event)
     {
         Stage newStage = new Stage();
-        
+
         NumberAxis y = new NumberAxis();
         CategoryAxis x = new CategoryAxis();
         LineChart l = new LineChart(x, y);
 
- 
         XYChart.Series<String, Double> series = new XYChart.Series<>();
         ArrayList<Attendance> allAttendance = s.getFullAttendance();
 
@@ -198,10 +193,20 @@ public class StudentViewController implements Initializable
         l.setTitle("Fraværshistorik");
         y.setLabel("Fravær i procent");
         x.setLabel("Antal skoledage");
-        Scene newScene = new Scene(new BorderPane(l));
+
+        
+        BorderPane bPane = new BorderPane();
+        bPane.setCenter(l);
+        
+        bPane.getStyleClass().add("background");
+
+        Scene newScene = new Scene(bPane);
         newStage.setHeight(600);
         newStage.setWidth(1000);
         newStage.setResizable(false);
+
+        newScene.getStylesheets().add("schoolapp/gui/view/Style.css");
+
         newStage.setScene(newScene);
         newStage.show();
 
